95a0fe12c32e9a87d5b888d47f270c5ba1a22afa
nathan-torresz <nathan_henrique2015@outlook.com>
2021-05-03 21:12:35 -0300

    App Criado

---
+++ Primeiro_App_Win_Forms_Teste/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Primeiro_App_Win_Forms_Teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.rdHabilitado = new System.Windows.Forms.RadioButton();
+            this.rdDesabilitado = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(270, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Se puder...";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(227, 314);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(321, 48);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Press and Hold";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button2.Font = new System.Drawing.Font("Bahnschrift", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Transparent;
+            this.button2.Location = new System.Drawing.Point(311, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(270, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(211, 48);
+            this.label2.TabIndex = 3;
+            this.label2.Text = " Prossiga! ";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button3.Font = new System.Drawing.Font("Bahnschrift", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Transparent;
+            this.button3.Location = new System.Drawing.Point(90, 180);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Continuar";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Enabled = false;
+            this.txtUserName.Font = new System.Drawing.Font("Bahnschrift", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUserName.Location = new System.Drawing.Point(227, 180);
+            this.txtUserName.MaxLength = 10;
+            this.txtUserName.Multiline = true;
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(321, 34);
+            this.txtUserName.TabIndex = 5;
+            this.txtUserName.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button4.Font = new System.Drawing.Font("Bahnschrift", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Transparent;
+            this.button4.Location = new System.Drawing.Point(357, 220);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(57, 26);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Travar";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Location = new System.Drawing.Point(554, 190);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // rdHabilitado
+            // 
+            this.rdHabilitado.AutoSize = true;
+            this.rdHabilitado.ForeColor = System.Drawing.Color.Transparent;
+            this.rdHabilitado.Location = new System.Drawing.Point(6, 21);
+            this.rdHabilitado.Name = "rdHabilitado";
+            this.rdHabilitado.Size = new System.Drawing.Size(92, 21);
+            this.rdHabilitado.TabIndex = 8;
+            this.rdHabilitado.TabStop = true;
+            this.rdHabilitado.Text = "Habilitado";
+            this.rdHabilitado.UseVisualStyleBackColor = true;
+            // 
+            // rdDesabilitado
+            // 
+            this.rdDesabilitado.AutoSize = true;
+            this.rdDesabilitado.ForeColor = System.Drawing.Color.Transparent;
+            this.rdDesabilitado.Location = new System.Drawing.Point(6, 48);
+            this.rdDesabilitado.Name = "rdDesabilitado";
+            this.rdDesabilitado.Size = new System.Drawing.Size(107, 21);
+            this.rdDesabilitado.TabIndex = 9;
+            this.rdDesabilitado.Text = "Desabilitado";
+            this.rdDesabilitado.UseVisualStyleBackColor = true;
+            this.rdDesabilitado.CheckedChanged += new System.EventHandler(this.rdDesabilitado_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdHabilitado);
+            this.groupBox1.Controls.Add(this.rdDesabilitado);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(590, 220);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(114, 78);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Digitação";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.RadioButton rdHabilitado;
+        private System.Windows.Forms.RadioButton rdDesabilitado;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Primeiro_App_Win_Forms_Teste/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primeiro_App_Win_Forms_Teste
+{
+    public partial class Form1 : Form
+    {
+        private bool travar;
+        public Form1()
+        {
+            InitializeComponent();
+            this.travar = false;
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label2.Hide();
+        }
+
+        private void button1_MouseUp(object sender, MouseEventArgs e)
+        {
+            label2.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_MouseEnter(object sender, EventArgs e)
+        {
+            if (!this.travar)
+            {
+                label2.Text = "HAHAHA!";
+                if (button3.Location.X < 430)
+                {
+                    button3.Location = new Point(430, 147);
+                }
+                else
+                {
+                    button3.Location = new Point(60, 147);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text.Equals("Nathan"))
+            {
+                this.travar = true;
+                MessageBox.Show("Botão Travado");
+                label2.Text = "Afff, droga ;-;";
+            }
+            else
+            {
+                MessageBox.Show("Usuário não encontrado!");
+                txtUserName.Clear();
+                txtUserName.Focus();
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            txtUserName.Visible = checkBox1.Checked;
+        }
+
+        private void rdDesabilitado_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rdDesabilitado.Checked) txtUserName.Enabled = false;
+            else txtUserName.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new Form2().Show();
+            this.Hide();
+        }
+    }
+}
